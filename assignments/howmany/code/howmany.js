be7b10518c5e86c7506e42bfa17559f5f0d0b34b
METROSTATE\36BDPA10 <likeakiel@gmail.com>
2017-03-11 13:57:20 -0600

    no message

---
+++ assignments/howmany/code/howmany.js
@@ -0,0 +1,17 @@
+function doLevel1(){
+	prompt("how many satelite dishes?")
+}
+function doLevel2(){
+	alert("apol")
+}
+function show(id){
+	var el = document.getElementaryId(id);
+	el.classList.remove("hidden");
+}
+function doLevel3(){
+	alert("apol")
+}
+function hide(id){
+	var el = document.getElementById(id);
+	el.classList.remove("hidden");
+}
